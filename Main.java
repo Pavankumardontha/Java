0f0a9cb0f74fe36643c83d0de1fb611090c4f6ba
Pavankumardontha <pavankumardontha@gmail.com>
2021-03-24 15:36:00 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+
+
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		Car porsche =  new Car(); //you have to declare and also initialise the object of a class
+		Car holden = new Car();
+		porsche.setModel("Carrera");
+		System.out.println("Model is "+ porsche.getModel());
+	}
+
+}
