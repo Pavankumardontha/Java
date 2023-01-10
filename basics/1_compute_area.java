a644c77b2d46b3215fefa9dd6a5dbd7be316ce0c
Pavankumardontha <pavankumardontha@gmail.com>
2023-01-11 01:05:48 +0530

    Create 1_compute_area.java

---
+++ basics/1_compute_area.java
@@ -0,0 +1,15 @@
+public class ComputeArea
+{
+  public static void main(String[] args)
+  {
+    //program to compute area of a circle
+    double radius;
+    double area;
+    //assign radius
+    radius = 20;
+    //compute area
+    area = radius*radius*3.14159;
+    //display results
+    System.out.println("The area for the circle of radius" + radius + "is" + area);
+  }
+}
